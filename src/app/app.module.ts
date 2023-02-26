99663885bcc75ae7dd1734982b122c6c8a83ddc2
ricardo mejias <panswer.rjmm@gmail.com>
2023-02-26 11:55:45 -0400

    desktop design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { TextTitleComponent } from './components/atoms/text-title/text-title.component';
@@ -20,7 +21,7 @@ import { ReactiveFormsModule } from '@angular/forms';
     ImageComponent,
     FaqComponentComponent,
   ],
-  imports: [BrowserModule, ReactiveFormsModule],
+  imports: [BrowserModule, ReactiveFormsModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/components/pages/faq-page/faq-page.component.ts
+++ src/app/components/pages/faq-page/faq-page.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 
 import { FAQItem } from '../../../interfaces/faq.interface';
-
 import { FaqDataService } from '../../../services/faq-data.service';
 
 @Component({
@@ -11,10 +10,25 @@ import { FaqDataService } from '../../../services/faq-data.service';
 })
 export class FaqPageComponent {
   FAQList: FAQItem[] = [];
+  
+  faqImageDesktop: string = '/assets/img/illustration-woman-online-desktop.svg';
+  faqImageMobile: string = '/assets/img/illustration-woman-online-mobile.svg';
+
+  faqImageBgDesktop: string = '/assets/img/bg-pattern-desktop.svg';
+  faqImageBgMobile: string = '/assets/img/bg-pattern-mobile.svg';
+
+  innerWidth: number = window.innerWidth;
 
   constructor(private faqDataService: FaqDataService) {}
 
   ngOnInit() {
     this.FAQList = this.faqDataService.getFAQs();
+    this.onResizeWindow();
+  }
+
+  onResizeWindow() {
+    window.addEventListener('resize', (e) => {
+      this.innerWidth = window.innerWidth;
+    });
   }
 }
